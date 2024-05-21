9f3c16999bbb8afefb5357d98f3fbb4403e8952b
Alex <an_koshelev@mail.ru>
2024-05-21 15:17:00 +0300

    add validation

--- api/schemas.py
+++ api/schemas.py
@@ -1,6 +1,10 @@
 import uuid
+import re
 
-from pydantic import BaseModel, ConfigDict
+from fastapi import HTTPException
+from pydantic import BaseModel, ConfigDict, field_validator
+
+LETTER_MATCH_PATTERN = re.compile(r"^[0-9]")
 
 
 class TunedModel(BaseModel):
@@ -16,8 +20,24 @@ class WriteData(BaseModel):
     phone: str
     address: str
 
+    @field_validator("phone")
+    def validate_phone(cls, value):
+        if not LETTER_MATCH_PATTERN.match(value):
+            raise HTTPException(
+                status_code=422, detail="Phone should contains only digit"
+            )
+        return value
+
 
 class WriteDataResponse(TunedModel):
     status: bool
     phone: str
     address: str
+
+    @field_validator("phone")
+    def validate_phone(cls, value):
+        if not LETTER_MATCH_PATTERN.match(value):
+            raise HTTPException(
+                status_code=422, detail="Phone should contains only digit"
+            )
+        return value
